ce82e61bcb68332d52e1fdbd010d671c73a73db4
hiroshima <hiroshima@hiroshimas-MacBook-Pro.local>
2024-10-25 18:04:28 +0300

    Add files

---
+++ homework01/affine.py
@@ -0,0 +1,24 @@
+def encrypt_affine(plaintext, a, b):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    m = len(alphabet)
+
+    def affine_encrypt_char(char, a, b):
+        if char.lower() not in alphabet:
+            return char
+        x = alphabet.index(char.lower())
+        encrypted_index = (a * x + b) % m
+        encrypted_char = alphabet[encrypted_index]
+        if char.isupper():
+            return encrypted_char.upper()
+        return encrypted_char
+
+
+    ciphertext = ''.join(affine_encrypt_char(char, a, b) for char in plaintext)
+    return ciphertext
+
+# Пример использования
+plaintext = "Hello, world!"
+a = 5
+b = 8
+ciphertext = encrypt_affine(plaintext, a, b)
+print(ciphertext)
